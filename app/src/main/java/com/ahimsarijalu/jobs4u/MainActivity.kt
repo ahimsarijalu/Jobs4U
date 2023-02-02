9b1278912207dec032edd14f91a4f843804d8a75
Rijalu Ahimsa <ahimsarijalu@gmail.com>
2023-02-02 11:05:15 +0700

    Enabling View Binding

--- app/src/main/java/com/ahimsarijalu/jobs4u/MainActivity.kt
+++ app/src/main/java/com/ahimsarijalu/jobs4u/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ahimsarijalu.jobs4u
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahimsarijalu.jobs4u.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
